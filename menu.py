b8b48065153ff3f25cfa1a4469d11d96feec0928
Karl <karl.effener@gmail.com>
2023-10-16 19:53:43 +0200

    Plusieurs choix dans le menu

---
+++ menu.py
@@ -0,0 +1,28 @@
+#!/bin/python
+
+def afficher_menu():
+	print("One vs One Tournament")
+	print("1. Start")
+	print("2. Crédit")
+	print("3. Quitter")
+
+def commencer_partie():
+	print("La partie commence !")
+
+def credit():
+	print("Jeu de combat tour par tour en un contre un. Le système est facile a comprendre donc pas de tuto")
+	print("Auteur: Kamel UNIQUEMENT il a triché. Karl a servi a rien la vérité")
+
+while True:
+	afficher_menu()
+	choix = input("Fait ton choix bombarde:   ")
+	if choix=="1":
+		commencer_partie()
+	elif choix=="2":
+		credit()
+	elif choix=="3":
+		print("Wa t'es nul Alt+F4")
+		break
+	else:
+		print("Ca me régale c'est soit 1 2 ou 3 mon reuf réveille toi !!!")
+
