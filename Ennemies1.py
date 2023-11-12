d8b9cd781a74600433401326c5c06d2890f2b288
anfaye4 <147809843+anfaye4@users.noreply.github.com>
2023-11-12 19:34:13 +0100

    correction de Ennemies1.py
    Ajout de double côte à Noob (ligne 13)
    "premier.nom" à la place de "premier" ( ligne 16)

--- Ennemies1.py
+++ Ennemies1.py
@@ -1,23 +1,22 @@
 #!/bin/python
 
 class Personnage:
-        def __init__ (self, nom, pv, end, mag):
-                self.nom = nom
-                self.pv = pv
-                self.end = end
-                self.mag = mag
+    def __init__(self, nom, pv, end, mag):
+        self.nom = nom
+        self.pv = pv
+        self.end = end
+        self.mag = mag
 
-#Creation du premier adversaire
+# Création du premier adversaire
 
+# Création d'une instance du personnage avec ses caractéristiques
+premier = Personnage("Noob", 50, 10, 5)
 
-#Creation instance  du perso avec ses caracteristiques
-premier=Personnage(Noob, 50, 10,5)
+# Annonce de l'ennemi
+print(f"Prochain adversaire: {premier.nom} !")
 
-#Annonce de l'ennemi
-print(f"Prochain adversaire: {premier} !")
-#Afficher les caracteristique du perso
+# Afficher les caractéristiques du personnage
 print(f"Nom: {premier.nom}")
 print(f"PV: {premier.pv}")
 print(f"END: {premier.end}")
 print(f"MAG: {premier.mag}")
-
